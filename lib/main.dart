3ac08a05932c169a291d88cb6dee1c5f4cb52417
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-14 11:05:52 +0530

    Changed the main page to SignUp for debug purposes

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_screen_layout.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
+import 'package:instagram_clone/screens/signup_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 void main() async {
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
       //   mobileScreenLayout: MobileScreenLayout(),
       //   webScreenLayout: WebScreenLayout(),
       // ),
-      home: const LoginScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
