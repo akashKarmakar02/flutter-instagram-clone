99ab3f04395d2a57b48fc73106cd5b24653df722
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-14 11:03:39 +0530

    Authentication Services File

---
+++ lib/services/AuthService.dart
@@ -0,0 +1,42 @@
+
+import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //sign up user
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    // required Uint8List file
+  }) async {
+    String res = "Some error occurred";
+    try {
+      if (email.isNotEmpty || password.isNotEmpty || username.isNotEmpty || bio.isNotEmpty) {
+        UserCredential credential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+
+        print(credential.user!.uid);
+
+        // add user to db
+        await _firestore.collection('users').doc(credential.user!.uid).set({
+          'uid': credential.user!.uid,
+          'username': username,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': []
+        });
+        res = "success";
+      }
+    } catch(e) {
+      res = e.toString();
+    }
+    return res;
+  }
+}
