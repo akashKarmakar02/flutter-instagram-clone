00e7de40a33c59e8f0499f971c234f628f6e6af4
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-18 17:48:25 +0530

    Added homeScreenWidget List in global variable file

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/add_post_screen.dart';
+
+const webScreenSize = 600;
+
+const homeScreenItems = [
+  Center(child: Text("Feed")),
+  Center(child: Text("Search")),
+  AddPostScreen(),
+  Center(child: Text("Notification")),
+  Center(child: Text("Profile")),
+];
