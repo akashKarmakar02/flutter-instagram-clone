aa093c05a39e0eb8d27ae0ba459d478d909f7366
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-18 16:29:54 +0530

    Changed to stateful widget

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user.dart' as model;
+import 'package:instagram_clone/provider/user_provider.dart';
+import 'package:provider/provider.dart';
 
-class MobileScreenLayout extends StatelessWidget {
-  const MobileScreenLayout({Key? key}) : super(key: key);
+class MobileScreenLayout extends StatefulWidget {
+  const MobileScreenLayout({super.key});
+
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    model.User? user = Provider.of<UserProvider>(context).getUser;
+
+    return Scaffold(
       body: Center(
-        child: Text('This is Mobile'),
+        child: Text(user == null ? "Loading": user.username),
       ),
     );
   }
