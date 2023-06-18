4b65661b49b903f2fcd39c445ad80a7975342a67
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-18 17:43:36 +0530

    UI for adding a post

---
+++ lib/screens/add_post_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
+    // return Center(
+    //   child: IconButton(
+    //     onPressed: () {},
+    //     icon: const Icon(Icons.upload_file),
+    //   ),
+    // );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(
+          icon: Icon(isIOS? Icons.arrow_back_ios_new : Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: const Text("Post to"),
+        actions: [
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Post",
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              )
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage('https://www.photoshopbuzz.com/wp-content/uploads/change-color-part-of-image-psd4.jpg'),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width*0.45,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Write a captain...",
+                    border: InputBorder.none,
+                  ),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                  aspectRatio: 487/451,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://www.photoshopbuzz.com/wp-content/uploads/change-color-part-of-image-psd4.jpg'),
+                        fit: BoxFit.fill,
+                        alignment: FractionalOffset.topCenter
+                      )
+                    ),
+                  ),
+                ),
+              ),
+              const Divider()
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
