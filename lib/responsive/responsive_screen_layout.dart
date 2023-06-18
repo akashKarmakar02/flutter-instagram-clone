b9192530447a66ad96ed4cbd90de858984ec8693
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-18 16:33:16 +0530

    Changed to a stateful widget

--- lib/responsive/responsive_screen_layout.dart
+++ lib/responsive/responsive_screen_layout.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/provider/user_provider.dart';
 import 'package:instagram_clone/utils/dimensions.dart';
+import 'package:provider/provider.dart';
 
-class ResponsiveLayout extends StatelessWidget {
+class ResponsiveLayout extends StatefulWidget {
   final Widget webScreenLayout;
   final Widget mobileScreenLayout;
 
   const ResponsiveLayout({Key? key, required this.webScreenLayout, required this.mobileScreenLayout}) : super(key: key);
 
   @override
+  State<ResponsiveLayout> createState() => _ResponsiveLayoutState();
+}
+
+class _ResponsiveLayoutState extends State<ResponsiveLayout> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    addData();
+  }
+
+  addData() async {
+    UserProvider _userProvider = Provider.of(context, listen: false);
+    await _userProvider.refreshUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
         builder: (context, constraints) {
           if(constraints.maxWidth > webScreenSize) {
-            return webScreenLayout;
+            return widget.webScreenLayout;
           }
-          return mobileScreenLayout;
+          return widget.mobileScreenLayout;
         }
     );
   }
