c30e67d8863c7679488f541e412004e6b725a3d8
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-23 11:24:57 +0530

    Added post logic

--- lib/services/StorageService.dart
+++ lib/services/StorageService.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 class StorageService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
@@ -11,6 +12,11 @@ class StorageService {
   // add image to firebase storage
   Future<String> uploadImageToStorage(String childName, Uint8List file, bool isPost) async {
     Reference reference = _storage.ref().child(childName).child(_auth.currentUser!.uid);
+
+    if (isPost) {
+      String id = const Uuid().v1();
+      reference = reference.child(id);
+    }
 
     UploadTask task = reference.putData(file);
     TaskSnapshot snapshot = await task;
