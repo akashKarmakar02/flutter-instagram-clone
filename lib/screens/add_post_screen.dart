1dc090267f5c3a76bdeb8fce2e5123a4b71652e8
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-23 11:21:18 +0530

    Added loading state for adding post and added functionality for back button

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:instagram_clone/models/user.dart';
 import 'package:instagram_clone/provider/user_provider.dart';
+import 'package:instagram_clone/services/FirestoreService.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/utils/utils.dart';
 import 'package:provider/provider.dart';
@@ -19,6 +20,47 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   Uint8List? _file;
   final TextEditingController _descriptionController = TextEditingController();
+  bool isLoading = false;
+
+
+  void postImage(
+      String uid,
+      String username,
+      String profImage
+  ) async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      String res = await FirestoreService().uploadPost(
+          _descriptionController.text,
+          _file!,
+          uid,
+          username,
+          profImage
+      );
+      setState(() {
+        isLoading = false;
+      });
+      if (res == "Success") {
+        showSnackBar('Posted', context);
+        clearImage();
+      } else {
+        showSnackBar(res, context);
+      }
+    } catch(e) {
+      setState(() {
+        isLoading = false;
+      });
+      showSnackBar(e.toString(), context);
+    }
+  }
+
+  void clearImage() {
+    setState(() {
+      _file = null;
+    });
+  }
 
   _selectImage(BuildContext context) async {
     bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
@@ -91,6 +133,70 @@ class _AddPostScreenState extends State<AddPostScreen> {
     );
   }
 
+  void onBackPressed(bool isIos) {
+    isIos? showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text('Are you sure?'),
+            content: const Text("If You Go Back The Post Will Be Deleted"),
+            actions: [
+              CupertinoDialogAction(
+                child: const Text("Discard"),
+                onPressed: () {
+                  setState(() {
+                    _file = null;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              CupertinoDialogAction(
+                child: const Text("Cancel"),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        }
+    ) : showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Are you sure?'),
+            content: const Text("If You Go Back The Post Will Be Deleted"),
+            actions: [
+              TextButton(
+                child: const Text("Discard"),
+                onPressed: () {
+                  setState(() {
+                    _file = null;
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              TextButton(
+                child: const Text("Cancel"),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final User? user = Provider.of<UserProvider>(context).getUser;
@@ -107,14 +213,14 @@ class _AddPostScreenState extends State<AddPostScreen> {
         backgroundColor: mobileBackgroundColor,
         leading: IconButton(
           icon: Icon(isIOS? Icons.arrow_back_ios_new : Icons.arrow_back),
-          onPressed: () {},
+          onPressed: () => onBackPressed(isIOS),
         ),
         title: const Text("Post to"),
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: TextButton(
-                onPressed: () {},
+                onPressed: () => postImage(user!.uid, user.username, user.photoUrl),
                 child: const Text(
                   "Post",
                   style: TextStyle(
@@ -129,6 +235,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
       ),
       body: Column(
         children: [
+          isLoading? const LinearProgressIndicator(): Container(),
           const SizedBox(height: 16),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
