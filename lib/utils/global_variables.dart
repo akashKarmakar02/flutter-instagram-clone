214ac212dbbf877eed48df94273efce8218cc076
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-26 22:47:52 +0530

    added search screen to homeScreenItems list

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screeen.dart';
+import 'package:instagram_clone/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Center(child: Text("Search")),
+  SearchScreen(),
   AddPostScreen(),
   Center(child: Text("Notification")),
   Center(child: Text("Profile")),
