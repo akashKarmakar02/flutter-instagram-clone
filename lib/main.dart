795cb84ae4f220bfcedad071f199401cb4400238
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-26 07:36:35 +0530

    Added material 3 design

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
         title: 'Instagram Clone',
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: mobileBackgroundColor,
+          useMaterial3: true,
         ),
         debugShowCheckedModeBanner: false,
         // home: const ResponsiveLayout(
