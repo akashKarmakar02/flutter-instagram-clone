c41ed855c5d7cdeb7d7433e09f6c5323ab87f744
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-28 22:49:15 +0530

    change homeScreenItems from const to final

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screeen.dart';
@@ -6,10 +7,10 @@ import 'package:instagram_clone/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
-  FeedScreen(),
-  SearchScreen(),
-  AddPostScreen(),
-  Center(child: Text("Notification")),
-  ProfileScreen(),
+final homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Center(child: Text("Notification")),
+  ProfileScreen(uid: FirebaseAuth.instance.currentUser?.uid,),
 ];
