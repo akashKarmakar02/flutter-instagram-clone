fb3b88f376acbfa3e3cb5fbfb76039e810689bca
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-28 22:51:02 +0530

    Covered the list tiles with gesture detector so that it can navigate to profile pages

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -54,11 +55,18 @@ class _SearchScreenState extends State<SearchScreen> {
           return ListView.builder(
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(snapshot.data!.docs[index].data()['photoUrl']),
+              return GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                  CupertinoPageRoute(
+                    builder: (context) => ProfileScreen(uid: snapshot.data!.docs[index].data()['uid'])
+                  )
                 ),
-                title: Text(snapshot.data!.docs[index].data()['username']),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(snapshot.data!.docs[index].data()['photoUrl']),
+                  ),
+                  title: Text(snapshot.data!.docs[index].data()['username']),
+                ),
               );
             },
           );
