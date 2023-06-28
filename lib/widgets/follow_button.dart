8ee71594959b1d0925871218c31e7aee7fed189c
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-28 09:25:25 +0530

    Button for profile screen

---
+++ lib/widgets/follow_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FollowButton extends StatelessWidget {
+  final Function()? function;
+  final Color backgroundColor;
+  final Color borderColor;
+  final Color textColor;
+  final String text;
+  const FollowButton({
+    super.key,
+    this.function,
+    required this.backgroundColor,
+    required this.borderColor,
+    required this.text,
+    required this.textColor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 4),
+      child: TextButton(
+        onPressed: function,
+        child: Container(
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            border: Border.all(
+              color: borderColor
+            ),
+            borderRadius: BorderRadius.circular(9),
+          ),
+          alignment: Alignment.center,
+          height: MediaQuery.of(context).size.height * 0.037,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
